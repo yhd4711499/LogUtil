10fa12ffea3a291aac12dd3e73f2c70a86351143
Ornithopter <yhd4711499@live.com>
2016-06-23 12:55:22 +0800

    fix a bug in LogTime comparing

--- src/LogTime.h
+++ src/LogTime.h
@@ -18,7 +18,7 @@ public:
     bool operator<( const LogTime& val ) const {
 		if (date.compare(val.date) < 0) {
 			return true;
-		} else if (time.compare(val.time) < 0){
+		} else if (date.compare(val.date) == 0 && time.compare(val.time) < 0){
 			return true;
 		}
 		return false;
